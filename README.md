e29527c8991341f862a166e3ef15d68f340766e9
Dessalines <dessalines@users.noreply.github.com>
2025-08-01 15:48:34 -0400

    Add recursive fetching to get proper date range (#11)
    * Adding a rustfmt file.
    
    * Adding recursive fetching to get date range.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Generate-dev-update
+
+Generates a lemmy, and lemmy-ui dev update for a given range.
+
+## Example
+
+`./run.sh "2025-07-01" "2025-08-01"`
--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,77 @@
 use anyhow::Result;
-use lemmy_api_common::{lemmy_db_schema::source::post::Post, post::GetPostsResponse};
+use chrono::{DateTime, NaiveDate, NaiveTime, Utc};
 use octocrab::{
-    models::pulls::PullRequest,
-    params::{pulls::Sort, Direction, State},
+  models::pulls::PullRequest,
+  params::{pulls::Sort, Direction, State},
 };
 
 /// Get list of pull requests from given repo under LemmyNet
-pub async fn list_prs(repo: &str) -> Result<Vec<PullRequest>> {
-    Ok(octocrab::instance()
-        .pulls("LemmyNet", repo)
-        .list()
-        .state(State::Closed)
-        .base("main")
-        .sort(Sort::Updated)
-        .direction(Direction::Descending)
-        .per_page(100)
-        .send()
-        .await?
-        .items)
+pub async fn list_prs(
+  repo: &str,
+  start_date: &DateTime<Utc>,
+  end_date: &DateTime<Utc>,
+) -> Result<Vec<PullRequest>> {
+  let mut results: Vec<PullRequest> = Vec::new();
+
+  let mut current_date = Utc::now();
+  let mut page = 1u32;
+
+  // Keep fetching until before start date
+  while current_date > *start_date {
+    let mut fetch_results = octocrab::instance()
+      .pulls("LemmyNet", repo)
+      .list()
+      .state(State::Closed)
+      .base("main")
+      .sort(Sort::Updated)
+      .direction(Direction::Descending)
+      .per_page(100)
+      .page(page)
+      .send()
+      .await?
+      .items;
+
+    // Set the current date and increase the page.
+    current_date = fetch_results
+      .last()
+      .map(|pr| pr.updated_at.unwrap_or_default())
+      .unwrap_or_default();
+    page += 1;
+
+    results.append(&mut fetch_results);
+  }
+
+  let filtered_results: Vec<PullRequest> = results
+    .into_iter()
+    // Filter results to the current range
+    .filter(|pr| {
+      pr.merged_at.unwrap_or_default() > *start_date && pr.merged_at.unwrap_or_default() < *end_date
+    })
+    // Ignore PRs with label `internal`
+    // TODO: apply this to refactoring changes and similar
+    .filter(|pr| {
+      pr.labels
+        .clone()
+        .unwrap()
+        .iter()
+        .all(|l| l.name != "internal")
+    })
+    // Ignore dependency updates
+    .filter(|pr| {
+      pr.user
+        .clone()
+        .map(|u| u.login != "renovate[bot]")
+        .unwrap_or(false)
+    })
+    .collect();
+
+  Ok(filtered_results)
 }
 
-// Use lemmy api to find last dev update post
-pub async fn last_dev_update() -> Result<Post> {
-    let client = reqwest::Client::builder()
-        .user_agent("generate-dev-update")
-        .build()?;
-    let url = "https://lemmy.ml/api/v3/post/list?limit=20&sort=New&type=Local&community_name=announcements";
-    let res = client
-        .get(url)
-        .send()
-        .await?
-        .json::<GetPostsResponse>()
-        .await?;
-    Ok(res
-        .posts
-        .into_iter()
-        .map(|p| p.post)
-        .find(|p| p.name.contains("Lemmy Development Update"))
-        .unwrap())
+pub fn string_to_utc(date_str: &str) -> Result<DateTime<Utc>> {
+  Ok(
+    NaiveDate::parse_from_str(date_str, "%Y-%m-%d")?
+      .and_time(NaiveTime::default())
+      .and_utc(),
+  )
 }
