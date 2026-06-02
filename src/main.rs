344dec1e07331743fff9c46e91f07b79335bb383
Nutomic <me@nutomic.com>
2026-06-02 16:59:45 +0200

    Group output by repo (#18)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
 use clap::Parser;
+use futures_util::future::try_join_all;
 use generate_dev_update::{list_prs, string_to_utc};
 use itertools::Itertools;
-use tokio::try_join;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -30,53 +30,37 @@ async fn main() -> Result<()> {
     end_date.date_naive()
   );
 
-  let (
-    lemmy_prs,
-    lemmy_ui_prs,
-    joinlemmy_prs,
-    jerboa_prs,
-    lemmy_js_client_prs,
-    lemmy_client_rs_prs,
-  ) = try_join!(
+  try_join_all([
     list_prs("lemmy", &start_date, &end_date),
     list_prs("lemmy-ui", &start_date, &end_date),
     list_prs("joinlemmy-site", &start_date, &end_date),
     list_prs("jerboa", &start_date, &end_date),
     list_prs("lemmy-js-client", &start_date, &end_date),
     list_prs("lemmy-client-rs", &start_date, &end_date),
-  )?;
-
-  let pull_requests = [
-    lemmy_prs,
-    lemmy_ui_prs,
-    joinlemmy_prs,
-    jerboa_prs,
-    lemmy_js_client_prs,
-    lemmy_client_rs_prs,
-  ]
-  .concat();
-
-  pull_requests
-    .into_iter()
-    .map(|pr| (pr.user.clone().unwrap().login, pr))
-    .sorted_by(|a, b| Ord::cmp(&b.0, &a.0))
-    // Group by author name
-    .chunk_by(|(author, _)| author.clone())
-    .into_iter()
-    .map(|chunk| (chunk.0, chunk.1.collect::<Vec<_>>()))
-    // Show authors with less PRs first
-    .sorted_by(|a, b| Ord::cmp(&a.1.len(), &b.1.len()))
-    // Print as markdown
-    .for_each(|pr| {
-      println!("\n## {}\n", pr.0);
-      for (_, pr) in pr.1 {
-        println!(
-          "- [{}]({})",
-          pr.title.clone().unwrap().trim(),
-          pr.html_url.unwrap().as_str()
-        );
-      }
-    });
+  ])
+  .await?
+  .into_iter()
+  .for_each(|pr| {
+    pr.into_iter()
+      .map(|pr| (pr.head.repo.clone().unwrap().name, pr))
+      .sorted_by(|a, b| Ord::cmp(&b.0, &a.0))
+      // Group by repo name
+      .chunk_by(|(repo, _)| repo.clone())
+      .into_iter()
+      .map(|chunk| (chunk.0, chunk.1.collect::<Vec<_>>()))
+      // Print as markdown
+      .for_each(|pr| {
+        println!("\n## {}\n", pr.0);
+        for (_, pr) in pr.1 {
+          println!(
+            "- [{}]({}) by @{}",
+            pr.title.clone().unwrap().trim(),
+            pr.html_url.clone().unwrap().as_str(),
+            pr.user.clone().unwrap().login
+          );
+        }
+      });
+  });
 
   println!("\n");
 
